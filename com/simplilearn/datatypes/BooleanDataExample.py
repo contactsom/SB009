9b1c397e1a065a853707389cefaa32ab716f1824
om <apipothi.com>
2022-11-28 12:22:15 +0530

    Python Day 1

---
+++ com/simplilearn/datatypes/BooleanDataExample.py
@@ -0,0 +1,20 @@
+print("This is the Boolean Data Type Example ")
+
+print("****** START ********")
+
+# 2>1 = true/false
+# 1<2 = true
+
+isTrue=10>20
+print(type(isTrue)) # <class 'bool'>
+print(isTrue) # False
+
+
+print("------------------------")
+
+isValid=30>20
+print(type(isValid)) # <class 'bool'>
+print(isValid) # True
+
+print("****** END ********")
+
---
+++ com/simplilearn/datatypes/TupleDataTypeExample.py
@@ -0,0 +1,15 @@
+print("This is the Tuple Data Type Example ")
+
+print("****** START ********")
+
+sessionLernerName=("Aditya","Sohali","Ankit") # String type Tuple
+print(type(sessionLernerName)) # <class 'tuple'>
+print(sessionLernerName)
+
+print("------------------------")
+
+sessionLernerPhone=(123,5687,98,35,778,858651)  # int type Tuple
+print(type(sessionLernerPhone)) # <class 'tuple'>
+print(sessionLernerPhone)
+
+print("****** END ********")
